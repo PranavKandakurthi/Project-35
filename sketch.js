c6bc688003998af71705da9b54975903225b34d7
PranavKandakurthi <72953403+PranavKandakurthi@users.noreply.github.com>
2020-12-21 18:34:26 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+//Create variables here
+var dog , happyDog , nomdog ;
+var food , foodstock
+var mm, ful, emty;
+
+var database;
+
+var food= 20
+function preload()
+{
+  //load images here
+  nomdog = loadImage("Dog.png");
+  happyDog = loadImage("happyDog.png");
+  emty = loadImage("Emty.png");
+  ful = loadImage("Met.png");
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  dog = createSprite(225,225,30,30)
+  dog.addImage("normal",nomdog);
+  dog.addImage("happy",happyDog)
+  dog.scale = 0.1
+  mm = createSprite(190,255,20,20);
+  mm.addImage("empty",emty);
+  mm.addImage("full",ful);
+  mm.scale = 0.1;
+  var foodstock = database.ref("food")
+  foodstock.on("value",readstock)
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87)
+  if(keyDown(UP_ARROW)){
+    
+   dog.changeImage("happy",happyDog) 
+    dog.scale = 0.1;
+    mm.changeImage("full",ful);
+    food = food -1;
+    writeStock(foods);
+}
+
+
+  drawSprites();
+
+  fill("white");
+  text("Food Remaining:"+ food ,140,400)
+
+  fill("white");
+  text("NOTE: Press the up arrow to give Don the Dog some Meat Mix!",140,50)
+  //add styles here
+
+}
+
+function readstock(data){
+  foods=data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1
+  }
+
+  database.ref('/').update({
+    food:x
+ })
+  
+}
